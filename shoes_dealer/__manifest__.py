da15b88fff72e4a62d92d0107511c8a16fa71ca3
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-19 18:16:46 +0200

    -referrer

--- shoes_dealer/__manifest__.py
+++ shoes_dealer/__manifest__.py
@@ -35,7 +35,8 @@
         "sale_product_image",
     ],
     "data": [
-#        "wizard/pnt_product_report_wizard_view.xml",
+        "views/menu_views.xml",
+        #"wizard/pnt_product_report_wizard_view.xml",
         "data/server_actions.xml",
         "security/ir.model.access.csv",
         "views/set_template_views.xml",
