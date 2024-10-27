0273388c6323b720c48d50445df9b9b97ad7b617
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-27 22:29:09 +0100

    shoes_color_chart

--- shoes_color_chart/models/shoes_color_chart_item.py
+++ shoes_color_chart/models/shoes_color_chart_item.py
@@ -9,10 +9,14 @@ class ShoesColorChartItem(models.Model):
     _description = 'Shoes color chart item'
 
     # Elementos de la tabla de relación de project => valores de atributo tipo color:
-    name = fields.Char('Name')
+    name = fields.Char('Name', compute='_get_color_chart_name')
     shoes_campaign_id = fields.Many2one('project.project', string='Campaign', domain="[('is_shoes_campaign','=',True)]")
     color_value_id = fields.Many2one('product.attribute.value', string="Color")
 
     # Estos dos los usaré para agrupar en la vista de "Items de paleta de colores":
     manufacturer_id = fields.Many2one('res.partner', related='color_value_id.partner_id')
     material_id = fields.Many2one('product.material', related='color_value_id.material_id')
+
+    def _get_color_chart_name(self):
+        ensure_one()
+        name = 'Hola'
