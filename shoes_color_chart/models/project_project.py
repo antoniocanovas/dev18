6d76a91e25a3920ced7b58eb7cf4a059b3078ed4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-28 08:44:03 +0100

    shoes_color_chart

--- shoes_color_chart/models/project_project.py
+++ shoes_color_chart/models/project_project.py
@@ -32,8 +32,8 @@ class ProjectProject(models.Model):
     def _check_manufacturer_and_material(self):
         for record in self:
             for li in record.shoes_color_value_ids:
-                if not li.partner_id.code or not li.material_id.code:
-                    raise UserError('Material and color CODES are required for all colors !!')
+                if not li.partner_id.ref or not li.material_id.code:
+                    raise UserError('Material, manufacturer and color CODES are required for all colors !!')
                 if not li.name:
                     campaign = record.name
                     manufacturer = li.partner_id.ref
