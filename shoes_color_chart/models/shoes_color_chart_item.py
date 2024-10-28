4e0c3d9b3ddcfbc250af9ca0adee0821f810985c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-28 09:02:02 +0100

    shoes_color_chart

--- shoes_color_chart/models/shoes_color_chart_item.py
+++ shoes_color_chart/models/shoes_color_chart_item.py
@@ -2,6 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 from odoo import fields, models, api
+from odoo.exceptions import UserError
 
 class ShoesColorChartItem(models.Model):
     _name = 'shoes.color.chart.item'
