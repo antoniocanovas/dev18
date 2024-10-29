f381d016af0bdeda2a9bb60cfd6b45ab4b001571
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-29 12:39:17 +0100

    shoes_color_chart v0

---
+++ shoes_color_chart/models/product_template_attribute_line.py
@@ -0,0 +1,10 @@
+# Copyright
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+
+from odoo import fields, models, api
+
+class ProductTemplateAttributeLine(models.Model):
+    _inherit = 'product.template.attribute.line'
+
+    color_value_ids = fields.Many2many(related='shoes_color_chart_id.color_value_ids')
