b6d934855ef79b25728121c2f592c0ec7743f9c9
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-29 15:46:26 +0100

    shoes_color_chart v0

--- shoes_color_chart/models/product_template.py
+++ shoes_color_chart/models/product_template.py
@@ -6,6 +6,5 @@ from odoo.exceptions import UserError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    shoes_color_chart_id = fields.Many2one('shoes.color.chart.item', related='shoes_task_id.shoes_color_chart_id')
-    material_id = fields.Many2one('product.material', related='shoes_task_id.shoes_color_chart_id.material_id')
-    manufacturer_id = fields.Many2one('res.partner', related='shoes_task_id.shoes_color_chart_id.manufacturer_id')
+    material_id = fields.Many2one('product.material', related='shoes_task_id.material_id')
+    manufacturer_id = fields.Many2one('res.partner', related='shoes_task_id.manufacturer_id')
