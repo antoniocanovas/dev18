d3cdf42c9ff2675ccae3bc46a00236cb8425a87a
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-29 12:44:34 +0100

    shoes_color_chart v0

--- shoes_color_chart/models/product_template_attribute_line.py
+++ shoes_color_chart/models/product_template_attribute_line.py
@@ -7,4 +7,4 @@ from odoo import fields, models, api
 class ProductTemplateAttributeLine(models.Model):
     _inherit = 'product.template.attribute.line'
 
-    color_value_ids = fields.Many2many(related='product_tmpl_id.shoes_color_chart_id.color_value_ids')
+    color_value_ids = fields.Many2many('product.attribute.value', related='product_tmpl_id.shoes_color_chart_id.color_value_ids')
