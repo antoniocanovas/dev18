41dbb395aafca6ca5a3c79decca1f0255e76720e
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-29 15:40:37 +0100

    shoes_color_chart v0

--- shoes_color_chart/models/product_template_attribute_line.py
+++ shoes_color_chart/models/product_template_attribute_line.py
@@ -7,4 +7,13 @@ from odoo import fields, models, api
 class ProductTemplateAttributeLine(models.Model):
     _inherit = 'product.template.attribute.line'
 
-    color_value_ids = fields.Many2many('product.attribute.value', related='product_tmpl_id.shoes_campaign_id.color_value_ids')
+
+    def _get_valid_product_attribute_values(self):
+        for record in self:
+            color_attribute = self.env.company.color_attribute_id
+            if record.attribute_id == color_attribute:
+                values = record.product_tmpl_id.shoes_campaign_id.color_attribute_ids.ids
+            else:
+                values = self.env['product.attribute.value'].search([('attribute_id','=',record.attribute_id.id)]).ids
+            record['campaign_value_ids'] = [(6,0,values)]
+    campaign_value_ids = fields.Many2many('product.attribute.value', compute='_get_valid_product_attribute_values')
