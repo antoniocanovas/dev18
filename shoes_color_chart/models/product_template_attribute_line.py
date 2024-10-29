aabecf128e73ac7cad4b6b156e4ee12a13333710
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-29 16:23:01 +0100

    shoes_color_chart v0

--- shoes_color_chart/models/product_template_attribute_line.py
+++ shoes_color_chart/models/product_template_attribute_line.py
@@ -16,8 +16,8 @@ class ProductTemplateAttributeLine(models.Model):
             if record.attribute_id == color_attribute:
                 chart_items = self.env['shoes.color.chart.item'].search([
                     ('manufacturer_id','=',record.product_tmpl_id.manufacturer_id.id),
-                    ('material_id','=',record.material_id),
-                    ('shoes_campaign_id','=',record.shoes_campaign_id.id)
+                    ('material_id','=',record.product_tmpl_id.material_id),
+                    ('shoes_campaign_id','=',record.product_tmpl_id.shoes_campaign_id.id)
                 ])
                 for li in chart_items:
                     values.add(li.color_value_id.id)
