b0d0b7339d52bccd3b357b855691302f6678f3eb
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-29 09:22:10 +0100

    shoes_color_chart v0

--- shoes_color_chart/models/shoes_color_chart_item.py
+++ shoes_color_chart/models/shoes_color_chart_item.py
@@ -13,3 +13,15 @@ class ShoesColorChartItem(models.Model):
     color_value_id = fields.Many2one('product.attribute.value', string="Color")
     manufacturer_id = fields.Many2one('res.partner', string="Manufacturer")
     material_id = fields.Many2one('product.material', string="Material")
+
+    @api.constrains('write_date')
+    def _avoid_duplicated(self):
+        for record in self:
+            exist = self.env['shoes.color.chart.item'].search([
+                ('material_id','=',record.material_id.id),
+                ('shoes_campaign_id','=',record.shoes_campaign_id.id),
+                ('manufacturer_id','=',record.manufacturer_id.id),
+                ('color_value_id','=',record.color_value_id.id),
+            ])
+            if exist.id:
+                raise UserError('This combination already exists: ' + record.name)
