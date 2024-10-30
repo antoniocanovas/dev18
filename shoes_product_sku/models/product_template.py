c82153259ef785f5e24927f27d485021feed4982
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-10-30 15:49:39 +0100

    sku

--- shoes_product_sku/models/product_template.py
+++ shoes_product_sku/models/product_template.py
@@ -6,3 +6,15 @@ from odoo.exceptions import UserError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+    def _update_product_product_sku(self):
+        for r in records:
+
+            if r.is_pair:
+                for product in r.product_variant_ids:
+
+                    a=1
+            elif r.is_assortment:
+                for product in r.product_variant_ids:
+                    a=1
+            else:
+                continue
