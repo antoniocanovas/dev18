d98926cc4e258888e9720421d670c11e9ebfe8c9
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-20 18:52:17 +0100

    shoes_campaign_ids

--- shoes_campaign/models/project_task.py
+++ shoes_campaign/models/project_task.py
@@ -25,14 +25,6 @@ class ProjectTask(models.Model):
         "shoes.pair.weight", string="Pair Weight", default=False
     )
 
-
-    # Cambia a IDS:
-    #material_id = fields.Many2one(
-    #    "product.material", string="Material", store=True, copy=True
-    #)
-    # Se incluye en shoes_model_material_ids:
-    #shoes_product_tmpl_id = fields.Many2one('product.template', string="Product", copy=False)
-
     shoes_model_material_ids = fields.One2many('shoes.model.material', 'task_id', string='Materials')
 
     intrastat_duty_id = fields.Many2one('intrastat.duty', string='Duty estimation', copy=False)
