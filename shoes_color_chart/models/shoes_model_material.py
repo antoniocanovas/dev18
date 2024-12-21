7e221f94fee9c39c005be266e7e1e147bb683691
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-21 17:46:56 +0100

    shoes_campaign_ids

--- shoes_color_chart/models/shoes_model_material.py
+++ shoes_color_chart/models/shoes_model_material.py
@@ -9,9 +9,11 @@ class ShoesModelMaterial(models.Model):
 
     # Valores heredados de la carta de color, PARA EL FABRICANTE DEL MODELO:
     def _get_manufacturer_campaign_materials(self):
-        materials = set()
-        for li in self.project_id.shoes_color_chart_item_ids:
-            if li.manufacturer_id == self.task_id.manufacturer_id:
-                materials.add(li.material_id.id)
-        self.material_value_ids = [(6,0,materials)]
-    material_value_ids = fields.Many2many('product.material', string='Materials', compute='_get_manufacturer_campaign_materials')
+        for record in self:
+            materials = set()
+            for li in record.task_id.project_id.shoes_color_chart_item_ids:
+                if li.manufacturer_id == record.task_id.manufacturer_id:
+                    materials.add(li.material_id.id)
+            record['material_value_ids'] = [(6,0,materials)]
+    material_value_ids = fields.Many2many('product.material', string='Materials',
+                                          compute='_get_manufacturer_campaign_materials')
