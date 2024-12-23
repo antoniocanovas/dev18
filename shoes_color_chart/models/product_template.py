9051c9ea759706db28cdcb54cd60bb842dcc988d
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-23 16:47:02 +0100

    creation wizard

--- shoes_color_chart/models/product_template.py
+++ shoes_color_chart/models/product_template.py
@@ -6,7 +6,6 @@ from odoo.exceptions import UserError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    material_id = fields.Many2one('product.material', related='shoes_task_id.material_id')
     manufacturer_id = fields.Many2one('res.partner', related='shoes_task_id.manufacturer_id')
 
     def update_color_values_from_campaign_chart_colors(self):
