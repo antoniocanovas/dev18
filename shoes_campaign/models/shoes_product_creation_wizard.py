41ee20cf98617cb5089c55dbe85c6a62d8dbc088
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-23 14:22:33 +0100

    creation wizard

--- shoes_campaign/models/shoes_product_creation_wizard.py
+++ shoes_campaign/models/shoes_product_creation_wizard.py
@@ -15,15 +15,7 @@ class ShoesProductCreationWizard(models.TransientModel):
     shoes_campaign_id = fields.Many2one(related='task_id.project_id')
     manufacturer_id = fields.Many2one(related='task_id.manufacturer_id')
 
-    #pending_product_material_ids = fields.Many2many(related='task_id.pending_product_material_ids')
-    #material_ids = fields.Many2many('product.material', string="Materials", required=True)
-
-    def _get_shoes_model_material_ids(self):
-        self.shoes_model_material_ids = [(6,0,self.task_id._get_shoes_model_material_ids.ids)]
-    shoes_model_material_ids = fields.Many2many('shoes.model.material', string='Model-material',
-                                                compute='_get_shoes_model_material_ids'
-                                                )
-
+    shoes_model_material_ids = fields.Many2many(related='task_id.shoes_model_material_ids')
     material_ids = fields.Many2many('shoes.model.material', name='Model materials')
 
     def action_apply(self):
