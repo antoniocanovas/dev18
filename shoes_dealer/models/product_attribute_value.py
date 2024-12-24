5b235ec71abddf465fa02f0aad38c908c2377ad6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-24 08:26:51 +0100

    gender

--- shoes_dealer/models/product_attribute_value.py
+++ shoes_dealer/models/product_attribute_value.py
@@ -8,6 +8,7 @@ class ProductAttributeValue(models.Model):
     _inherit = 'product.attribute.value'
 
     assortment_id = fields.Many2one('shoes.assortment', string='Template', store=True, copy=False)
+    gender = fields.Selection(related='assortment_id.gender')
 
     # Calcula si el atributo de conjunto debe estar oculto basado en la configuración de la compañía
     def _get_set_hidden(self):
