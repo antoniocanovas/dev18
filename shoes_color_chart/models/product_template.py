2e71c161ac08295b8a12b24d8b95e7ce15f612ab
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-24 09:05:43 +0100

    gender

--- shoes_color_chart/models/product_template.py
+++ shoes_color_chart/models/product_template.py
@@ -15,6 +15,7 @@ class ProductTemplate(models.Model):
                 colors = set()
                 chart_items = self.env['shoes.color.chart.item'].search(
                     [('shoes_campaign_id', '=', record.shoes_campaign_id.id),
+                     # Esto del material hay que revisarlo y cambiarlo por el atributo tipo color (como abajo):
                      ('material_id', '=', record.material_id.id)])
                 for li in chart_items:
                     colors.add(li.color_value_id.id)
@@ -33,23 +34,18 @@ class ProductTemplate(models.Model):
 
     def update_all_assortment_values_by_gender(self):
         for record in self:
-            if record.shoes_campaign_id.id and record.shoes_campaign_id.shoes_color_chart_item_ids.ids:
-                color_attribute = self.env.company.color_attribute_id
-                colors = set()
-                chart_items = self.env['shoes.color.chart.item'].search(
-                    [('shoes_campaign_id', '=', record.shoes_campaign_id.id),
-                     ('material_id', '=', record.material_id.id)])
-                for li in chart_items:
-                    colors.add(li.color_value_id.id)
+            if record.shoes_campaign_id.id:
+                assortment_attribute = self.env.company.assortment_attribute_id
+                assortments = self.env['product.attribute.value'].search(
+                    [('gender', '=', record.gender),
+                     ('attribute_id', '=', assortment_attribute.id)]).ids
 
                 ptal = self.env['product.template.attribute.line'].search(
-                    [('product_tmpl_id', '=', record.id), ('attribute_id', '=', color_attribute.id)])
+                    [('product_tmpl_id', '=', record.id), ('attribute_id', '=', assortment_attribute.id)])
                 if not ptal.id:
                     ptal = self.env['product.template.attribute.line'].create({
-                        'product_tmpl_id': record.id, 'attribute_id': color_attribute.id,
-                        'value_ids': [(6, 0, colors)]
+                        'product_tmpl_id': record.id, 'attribute_id': assortment_attribute.id,
+                        'value_ids': [(6, 0, assortments)]
                     })
                 else:
-                    ptal['value_ids'] = [(6, 0, colors)]
-            else:
-                raise UserError('Producto sin campaña o paleta de color.')
+                    ptal['value_ids'] = [(6, 0, assortments)]
