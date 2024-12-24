2f011b32180a1b30c1659756fb9fa40a7e80ee60
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2024-12-24 08:52:04 +0100

    gender

--- shoes_color_chart/models/product_template.py
+++ shoes_color_chart/models/product_template.py
@@ -24,7 +24,30 @@ class ProductTemplate(models.Model):
                 if not ptal.id:
                     ptal = self.env['product.template.attribute.line'].create({
                         'product_tmpl_id': record.id, 'attribute_id': color_attribute.id,
-                         'value_ids': [(6, 0, colors)]
+                        'value_ids': [(6, 0, colors)]
+                    })
+                else:
+                    ptal['value_ids'] = [(6, 0, colors)]
+            else:
+                raise UserError('Producto sin campaña o paleta de color.')
+
+    def update_all_assortment_values_by_gender(self):
+        for record in self:
+            if record.shoes_campaign_id.id and record.shoes_campaign_id.shoes_color_chart_item_ids.ids:
+                color_attribute = self.env.company.color_attribute_id
+                colors = set()
+                chart_items = self.env['shoes.color.chart.item'].search(
+                    [('shoes_campaign_id', '=', record.shoes_campaign_id.id),
+                     ('material_id', '=', record.material_id.id)])
+                for li in chart_items:
+                    colors.add(li.color_value_id.id)
+
+                ptal = self.env['product.template.attribute.line'].search(
+                    [('product_tmpl_id', '=', record.id), ('attribute_id', '=', color_attribute.id)])
+                if not ptal.id:
+                    ptal = self.env['product.template.attribute.line'].create({
+                        'product_tmpl_id': record.id, 'attribute_id': color_attribute.id,
+                        'value_ids': [(6, 0, colors)]
                     })
                 else:
                     ptal['value_ids'] = [(6, 0, colors)]
