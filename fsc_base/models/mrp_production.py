be7113ba44e00670ff1bac51d826f82fcda5e85f
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-04-28 11:01:49 +0200

    fsc_attributes_refresh

--- fsc_base/models/mrp_production.py
+++ fsc_base/models/mrp_production.py
@@ -13,7 +13,6 @@ class MrpProduction(models.Model):
         # c) Porcentaje directo del producto si es type MIX y el material no es por porcentaje fijo.
     fsc_percentage = fields.Float('FSC percentage')
 
-    @api.depends('state')
     def _get_fsc_efficiency_and_percentage(self):
         for record in self:
             # Para el cálculo de eficiencia FSC en base a pérdidas por corte y desechos:
@@ -22,26 +21,41 @@ class MrpProduction(models.Model):
             # Para el cálculo de porcentaje FSC en la entrada de producción que se guardará en el mrp.production:
             fscvolume, incomevolume  = 0, 0
 
-
             # move_raw_ids son entradas, move_finished_ids son todas las salidas, move_byproduct_ids sólo los subproductos:
             for sm in record.move_raw_ids:
                 if sm.product_id.material_id.fsc_tracking:
                     for sml in sm.move_line_ids:
                         factor = 1
-                        fsc_percentage = 100
+                        product = sml.product_id
+
+                        # Para productos FSC 100%:
+                        if product.fsc_type in ['fsc', 'recycled']:
+                            fsc_percentage = 100
+                        # Caso de que el % sea estimado y responsabilidad del cliente en compras y fabricaciones:
+                        elif material.fsc_mix_estimation:
+                            fsc_percentage = material.fsc_mix_percentage
+                        # Caso de producto comprado con un % certificado de FSC pero el % cambiará al mezclar en fabricación:
+                        elif not material.fsc_mix_estimation and product.fsc_type in ['mix_credit', 'mix_recycled']:
+                            fsc_percentage = product.fsc_percentage
+
                         # Buscamos la orden de producción para este producto y lote por si encontramos eficiencia previa:
                         smlproduced = self.env['stock.move.line'].search([
-                            ('product_id', '=', sml.product_id.id),
+                            ('product_id', '=', product.id),
                             ('move_id.production_id', '!=', False),
                             ('location_id.usage', '=', 'production'),
                             ('lot_id', '=', sml.lot_id.id),
                         ], limit=1)
                         if smlproduced.id and smlproduced.move_id.production_id.fsc_efficiency > 0:
                             factor = smlproduced.move_id.production_id.fsc_efficiency / 100
+
+                            # Para el caso de productos que puedan fluctuar su certificación FSC:
+                        if smlproduced.id and product.fsc_type in ['mix_credit', 'mix_recycled']:
                             fsc_percentage = smlproduced.move_id.production_id.fsc_percentage
-                        rawvolume += sml.product_id.volume * sml.quantity / factor
-                        incomevolume += sml.product_id.volume * sml.quantity
-                        fscvolume += sml.product_id.volume * sml.quantity * fsc_percentage / 100
+
+                        rawvolume += product.volume * sml.quantity / factor
+                        incomevolume += product.volume * sml.quantity
+                        fscvolume += product.volume * sml.quantity * fsc_percentage / 100
+
             fsc_percentage = fscvolume / incomevolume * 100
             print('fsc_percentage: ' + str(fsc_percentage) + " fscvolume: " + str(fscvolume)+ " incomevolume: " + str(incomevolume))
 
@@ -53,10 +67,13 @@ class MrpProduction(models.Model):
                 efficiency = producedvolume / rawvolume * 100
             record.write({'fsc_efficiency': efficiency, 'fsc_percentage':fsc_percentage})
 
-    @api.constrains('state')
-    def _avoid_fsc_smaller_100_in_fsc100_products(self):
+    def _fsc_update_mrp_update(self):
         for rec in self:
-            if rec.state == 'done' and rec.fsc_efficiency < 100:
+            # Actualizar datos de eficiencia:
+            rec._get_fsc_efficiency_and_percentage()
+
+            # Chequear si los productos finales son 100% FSC y los de entrada son también 100%:
+            if rec.state == 'done' and rec.fsc_percentage < 100:
                 products = rec.finished_move_line_ids.product_id
                 product_names = ""
                 for product in products:
