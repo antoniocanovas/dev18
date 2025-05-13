693f58dda0d2c11ec60ea3a116fa498c2d23c93c
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-05-13 18:54:43 +0200

    fsc v3

--- fsc_base/models/__init__.py
+++ fsc_base/models/__init__.py
@@ -3,7 +3,7 @@ from . import product_material
 from . import mrp_production
 from . import stock_lot
 from . import product
-from . import fsc_audit
 from . import fsc_audit_line
-from . import eutr_audit
+from . import fsc_audit
 from . import eutr_audit_line
+from . import eutr_audit
--- fsc_base/models/eutr_audit_line.py
+++ fsc_base/models/eutr_audit_line.py
@@ -5,8 +5,8 @@
 from odoo import fields, models, api
 
 class EutrAuditLine(models.Model):
-    _name = 'eudr.audit.line'
-    _description = 'EUDR audit line'
+    _name = 'eutr.audit.line'
+    _description = 'EUTR audit line'
 
 
     product_id = fields.Many2one('product.product', string='Product')
@@ -14,9 +14,9 @@ class EutrAuditLine(models.Model):
     eutr_audit_id = fields.Many2one('eutr.audit', string='EUTR Audit')
 
     intrastat_code_id = fields.Many2one(related='product_id.intrastat_code_id', string='Intrastat')
-    eudr_nc_code = fields.Char('NC', related='product_id.eudr_nc_code')
+    eutr_nc_code = fields.Char('NC', related='product_id.eutr_nc_code')
     qty      = fields.Float('Quantity (TM)')
     state_id = fields.Many2one('res.country.state', string='State')
     country_id = fields.Many2one('res.country', string='Country')
-    eudr_cdc = fields.Boolean('CDC', help='Custody chain')
-    eudr_cl  = fields.Boolean('CL', help='Legal control')
+    eutr_cdc = fields.Boolean('CDC', help='Custody chain')
+    eutr_cl  = fields.Boolean('CL', help='Legal control')
