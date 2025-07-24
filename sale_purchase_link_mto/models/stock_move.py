a6ac5779892c22c3685256d1655500b7209057f6
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-07-24 14:13:22 +0200

    salepurchaselinkmto

--- sale_purchase_link_mto/models/stock_move.py
+++ sale_purchase_link_mto/models/stock_move.py
@@ -2,9 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
-import logging
-
-_logger = logging.getLogger(__name__)
 
 
 class StockMove(models.Model):
@@ -24,26 +21,5 @@ class StockMove(models.Model):
         # Si el move tiene sale_line_id, lo propagamos al procurement
         if self.sale_line_id:
             values['sale_line_id'] = self.sale_line_id.id
-            _logger.debug(
-                "Propagating sale_line_id %s from stock.move %s to procurement values",
-                self.sale_line_id.id, self.id
-            )
         
         return values
-
-    def _action_confirm(self, merge=True, merge_into=False):
-        """
-        Override para debug - ayuda a rastrear el flujo de datos
-        """
-        result = super()._action_confirm(merge=merge, merge_into=merge_into)
-        
-        # Log para debugging en caso de problemas
-        moves_with_sale = self.filtered('sale_line_id')
-        if moves_with_sale:
-            _logger.debug(
-                "Confirmed %d stock moves with sale_line_id: %s", 
-                len(moves_with_sale), 
-                moves_with_sale.mapped('sale_line_id.id')
-            )
-        
-        return result
--- sale_purchase_link_mto/models/stock_rule.py
+++ sale_purchase_link_mto/models/stock_rule.py
@@ -2,9 +2,6 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import models
-import logging
-
-_logger = logging.getLogger(__name__)
 
 
 class StockRule(models.Model):
@@ -33,61 +30,5 @@ class StockRule(models.Model):
         
         if move_to_copy.sale_line_id:
             res['sale_line_id'] = move_to_copy.sale_line_id.id
-            _logger.debug(
-                "Copying sale_line_id %s from move %s to new move via push rule",
-                move_to_copy.sale_line_id.id, move_to_copy.id
-            )
         
         return res
-
-    def _run_buy(self, procurements):
-        """
-        Override con logging simplificado y manejo de errores robusto.
-        """
-        try:
-            # Intento de logging seguro - si falla, continúa sin problemas
-            count_with_sale = 0
-            for procurement, rule in procurements:
-                if (hasattr(procurement, 'values') and 
-                    isinstance(procurement.values, dict) and 
-                    procurement.values.get('sale_line_id')):
-                    count_with_sale += 1
-            
-            if count_with_sale > 0:
-                _logger.debug(
-                    "Running buy rule for %d procurements with sale_line_id",
-                    count_with_sale
-                )
-        except Exception as e:
-            # Logging falla, pero no interrumpimos la funcionalidad principal
-            _logger.warning("Could not log procurement info: %s", str(e))
-        
-        return super()._run_buy(procurements)
-
-
-class ProcurementGroup(models.Model):
-    _inherit = 'procurement.group'
-
-    def run(self, procurements, raise_user_error=True):
-        """
-        Override con logging simplificado y manejo de errores robusto.
-        """
-        try:
-            # Intento de logging seguro - si falla, continúa sin problemas
-            count_with_sale = 0
-            for procurement in procurements:
-                if (hasattr(procurement, 'values') and 
-                    isinstance(procurement.values, dict) and 
-                    procurement.values.get('sale_line_id')):
-                    count_with_sale += 1
-            
-            if count_with_sale > 0:
-                _logger.debug(
-                    "Processing %d procurements with sale_line_id in group run",
-                    count_with_sale
-                )
-        except Exception as e:
-            # Logging falla, pero no interrumpimos la funcionalidad principal
-            _logger.warning("Could not log procurement group info: %s", str(e))
-        
-        return super().run(procurements, raise_user_error=raise_user_error)
