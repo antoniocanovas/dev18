42dbb7105baf5fd7978926a3b287301808039bc4
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-11-05 19:21:38 +0100

    margen en pricelist, duty en sd

---
+++ intrastat_duty/models/__init__.py
@@ -0,0 +1,5 @@
+from . import intrastat_duty
+from . import product_template
+from . import product_product
+from . import product_pricelist_item
+from . import purchase_order
---
+++ intrastat_duty/models/purchase_order.py
@@ -0,0 +1,21 @@
+# Copyright
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    def _get_duty_estimation(self):
+        for record in self:
+            total = 0
+            for li in record.order_line:
+                if li.product_id.product_tmpl_id.intrastat_duty_id.id:
+                    tax_percent = (
+                        li.product_id.product_tmpl_id.intrastat_duty_id.duty / 100
+                    )
+                    total += li.product_qty * li.price_unit * tax_percent
+            record["duty_estimation"] = total
+
+    duty_estimation = fields.Monetary("Duty estimation", compute="_get_duty_estimation")
