2349c0031ce2e467ba64d79aef18d93447205722
antoniocanovas <antonio.canovas@ingenieriacloud.com>
2025-11-05 18:44:12 +0100

    delete

--- intrastat_duty/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import intrastat_duty
-from . import product_template
-from . import product_product
-from . import product_pricelist_item
-from . import purchase_order
--- shoes_campaign/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import project_project
-from . import project_task
-from . import product_template
-from . import shoes_model_material
-from . import shoes_product_creation_wizard
-from . import shoes_type
-from . import shoes_closure
